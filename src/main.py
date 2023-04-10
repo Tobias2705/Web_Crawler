5b507f09a1fe2946f311684d2847747d07d31327
Jakub Kaczmarek <kaczmareksg@gmail.com>
2023-04-10 23:42:33 +0200

    update structure

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from component_1 import RegonDatabaseScrapper
-
-input_data_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'input_data'))
-test_data_path = os.path.join(input_data_path, 'test_data.txt')
-scraper = RegonDatabaseScrapper(test_data_path)
-
-regon_db = scraper.get_entity_info()
-print(regon_db[['nip', 'regon', 'nazwa', 'wojewodztwo']])
