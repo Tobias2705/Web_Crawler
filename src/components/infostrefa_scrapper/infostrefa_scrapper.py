a91c037dcca160e67fd425a84a663980c50e77a9
Miligro <milo20054@gmail.com>
2023-05-17 22:45:44 +0200

    Add bankier and stock name scraper

--- src/components/infostrefa_scrapper/infostrefa_scrapper.py
+++ src/components/infostrefa_scrapper/infostrefa_scrapper.py
@@ -1,11 +1,7 @@
-import time
 import pandas as pd
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as firefoxOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
 from bs4 import BeautifulSoup
 
 
@@ -14,10 +10,7 @@ class InfoStrefaScrapper:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        firefox_options = firefoxOptions()
-        firefox_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.firefoxDriver = webdriver.Firefox(options=firefox_options)
         self.driver.implicitly_wait(30)
         self.news_links = []
         self.entities = entities
@@ -51,8 +44,7 @@ class InfoStrefaScrapper:
         for count, entity in enumerate(self.entities.itertuples()):
             counter = str(count+1) + '/' + str(len(self.entities))
             try:
-                entity_isin = self._get_entity_isin(entity.nazwa)
-                entity_id = self._get_entity_id(entity_isin)
+                entity_id = self._get_entity_id(entity.nazwa_gieldowa)
                 if entity_id:
                     self.driver.get(
                         f"https://infostrefa.com/infostrefa/pl/wiadomosci/szukaj/1?company={entity_id}&category=wszystko")
@@ -67,7 +59,6 @@ class InfoStrefaScrapper:
                     print(f"{counter} InfostrefaScraper could not scrap: {entity.nip}")
 
         self.driver.quit()
-        self.firefoxDriver.quit()
         return self.news
 
     def _get_entity_id(self, entity: str):
@@ -82,7 +73,7 @@ class InfoStrefaScrapper:
                 id = td.find('a')['href'].split('/')[-1].split(',')[0]
                 return id
 
-    def _get_news(self, entity_name: str):
+    def _get_news(self, entity: str):
         for i in range(len(self.news_links)):
             self.driver.get(f"https://infostrefa.com{self.news_links[i]}")
             html = self.driver.page_source
@@ -104,14 +95,4 @@ class InfoStrefaScrapper:
                             else:
                                 text += '\n'
             date = soup.find('div', {'class': 'text-date'}).text
-            self.news.loc[len(self.news)] = [entity_name, date, text]
-
-    def _get_entity_isin(self, entity_name: str):
-        entity_name = entity_name.replace('"', '')
-        self.firefoxDriver.get("https://www.gpw.pl/spolki")
-        search_input = self.firefoxDriver.find_element(By.NAME, 'searchText')
-        time.sleep(0.5)
-        search_input.send_keys(entity_name)
-        WebDriverWait(self.firefoxDriver, 60).until(ec.invisibility_of_element_located((By.ID, 'preview-area')))
-        entity_link = self.firefoxDriver.find_element(By.XPATH, "//tbody[contains(@id, 'search-result')]/tr/td/a").get_attribute('href')
-        return entity_link.split('=')[-1]
+            self.news.loc[len(self.news)] = [entity, date, text]
